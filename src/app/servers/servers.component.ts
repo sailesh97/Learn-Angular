18c78f494c38a870bca4e0783dfcac3108f17b8d
Sailesh Patra <saileshpatra997@gmail.com>
2021-07-09 01:42:08 +0530

    Added styles to our component

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,8 +2,9 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  templateUrl: './servers.component.html', 
+  // template: `<app-servers></app-servers>`
+  styleUrls: ['./servers.component.css'] // As it's an array, you can use multiple stylesheets
 })
 export class ServersComponent implements OnInit {
 
@@ -13,3 +14,14 @@ export class ServersComponent implements OnInit {
   }
 
 }
+
+/**
+ *  One of the two templateUrl or template have to be present here(mandatory). 
+ * 
+ * You can omit the selector. We can load a component without a selctor using a routing as well.
+ * 
+ * You can skip the styles as well.
+ * 
+ * But any one of template or templateUrl must be there
+ * 
+ */
