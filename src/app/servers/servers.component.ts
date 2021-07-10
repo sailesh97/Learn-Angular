e8933e1fa009a988326975dd98b2be4a059e9d6a
Sailesh Patra <saileshpatra997@gmail.com>
2021-07-10 22:31:57 +0530

    Different ways of using selector property inside component decorator

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-servers',
+  // selector: 'app-servers',
+  // selector: '[app-servers]',
+  selector: '.app-servers',
   templateUrl: './servers.component.html', 
   // template: `<app-servers></app-servers>`
   styleUrls: ['./servers.component.css'] // As it's an array, you can use multiple stylesheets
@@ -24,4 +26,17 @@ export class ServersComponent implements OnInit {
  * 
  * But any one of template or templateUrl must be there
  * 
+ */
+
+ /** 
+  * Rules for selector property:
+  *   1. It should be unique and should not override with any existing HTML Element Name
+  *   2.  
+  *     i.   HTML Selector --> 1st Way: selector: 'app-servers' ---> In HTML File <app-servers></app-servers>
+  *     ii.  Attribute Selector ---> 2nd Way: selector: '[app-servers]' -----> In html file <div app-servers></div>
+  *     iii. CSS Selector ---> 3rd Way: selector: '.app-servers' -----> In HTML file <div class="app-servers"></div>
+  * 
+  *   3. Selecting By ID won't work.
+  *   4. Pseudo selection won't work.
+  * 
  */
