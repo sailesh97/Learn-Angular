0b9f96bd6394712e2a1fa27069b1c42d518c2aec
Sailesh Patra <saileshpatra997@gmail.com>
2021-07-11 01:35:11 +0530

    Introducton to Data Binding(Property Binding)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,8 +9,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css'] // As it's an array, you can use multiple stylesheets
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
+  allowNewServer = false;
+  constructor() { 
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000)
+  }
 
   ngOnInit(): void {
   }
