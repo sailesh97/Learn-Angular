8a4ca783f1f3681472fc343c88593500961dfa90
Sailesh Patra <saileshpatra997@gmail.com>
2021-07-11 11:47:53 +0530

    Introduction to ngStyle directive

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,7 +10,16 @@ export class ServerComponent{
     serverId: number = 10; // Though It's a number, It can be easily converted to string. So it will work in string interpolation.
     serverStatus:string = 'offline'; 
 
+    constructor(){
+        this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+        // Based on which we will change the background color of the elements.
+    }
+
     getServerStatus(){
         return this.serverStatus;
+    }
+
+    getColor(){
+        return this.serverStatus === 'online' ? 'green' : 'red';
     }
 }
